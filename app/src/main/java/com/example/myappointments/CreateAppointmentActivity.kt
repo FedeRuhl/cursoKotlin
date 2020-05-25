e04c2cd3b8ef7591f4a1300acb2cd244bf21cf58
Federico <federicoruhl@gmail.com>
2020-05-25 12:06:58 -0300

    first commit

---
+++ app/src/main/java/com/example/myappointments/CreateAppointmentActivity.kt
@@ -0,0 +1,99 @@
+package com.example.myappointments
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_appointment.*
+import java.util.*
+
+class CreateAppointmentActivity : AppCompatActivity() {
+
+    private val selectedCalendar = Calendar.getInstance()
+    private var selectedRadioButton: RadioButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_appointment)
+
+        btnNext.setOnClickListener{
+            cvStep1.visibility = View.GONE;
+            cvStep2.visibility = View.VISIBLE;
+        }
+
+        btnConfirm.setOnClickListener{
+            Toast.makeText(this, "Turno registrado correctamente", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+        val specialtyOptions = arrayOf("Specialty A", "Specialty B", "Specialty C")
+        spinnerSpecialties.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, specialtyOptions)
+
+        val doctorOptions = arrayOf("Doctor A", "Doctor B", "Doctor C")
+        spinnerDoctors.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, doctorOptions)
+
+    }
+
+    fun onClickScheduledDate(v: View?){
+        val year = selectedCalendar.get(Calendar.YEAR)
+        val month = selectedCalendar.get(Calendar.MONTH)
+        val dayOfMonth = selectedCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val listener = DatePickerDialog.OnDateSetListener { view, y, m, d ->
+            //Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()
+            selectedCalendar.set(y, m, d)
+            etScheduledDate.setText(
+                resources.getString(R.string.date_format,
+                    y,
+                    m.twoDigits(),
+                    d.twoDigits()
+            ))
+            displayRadioButtons()
+        }
+
+        val datePickerDialog = DatePickerDialog(this, listener, year, month, dayOfMonth)
+
+        val datePicker = datePickerDialog.datePicker
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+        datePicker.minDate = calendar.timeInMillis
+        calendar.add(Calendar.MONTH, 1)
+        datePicker.maxDate = calendar.timeInMillis
+        datePickerDialog.show()
+    }
+
+    private fun displayRadioButtons(){
+//        rgScheduledTime.clearCheck()
+//        rgScheduledTime.removeAllViews()
+        rgScheduledTimeLeft.removeAllViews()
+        rgScheduledTimeRight.removeAllViews()
+
+        selectedRadioButton = null
+
+        val hours = arrayOf("15:00", "15:30", "14:00", "14:30")
+        var goToLeft = true
+
+        hours.forEach {
+            val radioButton = RadioButton(this)
+            radioButton.id = View.generateViewId()
+            radioButton.text = it
+            radioButton.setOnClickListener{
+                selectedRadioButton?.isChecked = false
+                selectedRadioButton = it as RadioButton?
+                selectedRadioButton?.isChecked = true
+            }
+            if (goToLeft)
+                rgScheduledTimeLeft.addView(radioButton)
+            else
+                rgScheduledTimeRight.addView(radioButton)
+
+            goToLeft = !goToLeft
+        }
+
+    }
+
+    private fun Int.twoDigits() = if (this>9) this.toString() else "0$this" //extension function
+}
---
+++ app/src/main/java/com/example/myappointments/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myappointments
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        tvGoToRegister.setOnClickListener{
+            Toast.makeText(this,getString(R.string.please_fill_your_register_data), Toast.LENGTH_SHORT).show() //mensaje que se muestra y rapidamente desaparece
+
+            val intent = Intent(this, RegisterActivity::class.java) //Intent es una clase java
+            startActivity(intent)
+        }
+    }
+}
