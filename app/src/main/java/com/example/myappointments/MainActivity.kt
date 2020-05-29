3c8e0552025836329cdb1016af79f189de26377d
Federico <federicoruhl@gmail.com>
2020-05-29 11:48:55 -0300

    add snackBar and override fun onBackPressed

--- app/src/main/java/com/example/myappointments/MainActivity.kt
+++ app/src/main/java/com/example/myappointments/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.myappointments
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import com.example.myappointments.PreferenceHelper.get
+import com.example.myappointments.PreferenceHelper.set
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val snackBar by lazy { //se inicializa en null pero cuando se la requiera, recien ahi se va a inicializar
+        Snackbar.make(mainLayout, R.string.press_back_agaian, Snackbar.LENGTH_SHORT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        persistir datos: shared preferences, sqlite, files con distintos fines
+//        val preferences = getSharedPreferences("general", Context.MODE_PRIVATE)
+//        val session = preferences.getBoolean("session", false) //si la variable no se ha creado, devuelve falso
+
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        if (preferences["session", false])
+            goToMenuAcitivity()
+
         btnLogin.setOnClickListener{
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
-            finish()
-             //finaliza con main activity
+            //deberia validar datos con el servidor
+            createSessionPreference()
+            goToMenuAcitivity()
         }
 
         tvGoToRegister.setOnClickListener{
@@ -26,5 +41,28 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java) //Intent es una clase java
             startActivity(intent)
         }
+    }
+
+    private fun goToMenuAcitivity(){
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+        finish()
+        //finaliza con main activity
+    }
+
+    private fun createSessionPreference(){
+//        val preferences = getSharedPreferences("general", Context.MODE_PRIVATE)
+//        val editor = preferences.edit()
+//        editor.putBoolean("session", true)
+//        editor.apply()
+        val preferences = PreferenceHelper.defaultPrefs(this)
+        preferences["session"] = true
+    }
+
+    override fun onBackPressed() {
+        if (snackBar.isShown)
+            super.onBackPressed()
+        else
+            snackBar.show()
     }
 }
